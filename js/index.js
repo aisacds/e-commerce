6c45bef2cb785fb2fe168f1352eb4ffa0083e9d1
aisacds <isaac4747ds@gmail.com>
2022-08-21 15:48:29 -0300

    add login

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
+        
     });
     document.getElementById("juguetes").addEventListener("click", function() {
         localStorage.setItem("catID", 102);
@@ -12,3 +13,7 @@ document.addEventListener("DOMContentLoaded", function(){
         window.location = "products.html"
     });
 });
+
+if (!localStorage.getItem('email')) {
+    window.location.href = "login.html";
+}
---
+++ js/login.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let email = document.getElementById("input-email");
+    let pw = document.getElementById("input-password");
+    if (email.value.length !== 0 && pw.value.length !== 0) {
+        localStorage.setItem('email', email.value);
+        localStorage.setItem('pw', pw.value);
+        window.location.href = "index.html";
+
+    } else {
+        alert("Debes ingresar");
+    }
+})
