7118d478ebc4cda19aec8557815fa2282fbdf572
aisacds <isaac4747ds@gmail.com>
2022-09-11 14:13:03 -0300

    Login google delete

--- js/login.js
+++ js/login.js
@@ -14,22 +14,3 @@ form.addEventListener("submit", function (e) {
     }
 })
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('Name: ' + profile.getName());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
-
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
-
-  window.gapi.client
-        .init({
-          clientId:'261059380792-sngr9a7v5mvr0du6ef8a3acnqmfnh796.apps.googleusercontent.com',
-          scope: "email",
-          plugin_name:'JapLogin'
-        })
