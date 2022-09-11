3b4c05bce083f6c8ec0cdb2f25ae660867845ce0
aisacds <isaac4747ds@gmail.com>
2022-09-11 12:32:10 -0300

    loginGoogle3

--- js/login.js
+++ js/login.js
@@ -13,3 +13,16 @@ form.addEventListener("submit", function (e) {
         alert("Debes ingresar");
     }
 })
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('Name: ' + profile.getName());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
